77e327b70c53f7482fe548f08b5ad98cc4a0c3dc
Arvin Buid <arvinbuid01@gmail.com>
2025-03-10 23:18:07 +0800

    Get all jobs

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
 
+import {nanoid} from "nanoid";
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,15 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+let jobs = [
+  {id: nanoid(), name: "Software Engineer", company: "Google"},
+  {id: nanoid(), name: "Web Developer", company: "Microsoft"},
+];
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({jobs});
+});
 
 app.post("/", (req, res) => {
   console.log(req.body);
