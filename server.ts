b4c2263f8585fc7b0fd7c6ca418476c1e6ac236b
Arvin Buid <arvinbuid01@gmail.com>
2025-03-10 22:28:39 +0800

    Morgan and dotenv.

--- server.ts
+++ server.ts
@@ -1,12 +1,21 @@
+import dotenv from "dotenv";
 import express from "express";
+import morgan from "morgan";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.post("/", (req, res) => {
   console.log(req.body);
   res.json({message: "Data received", data: req.body});
 });
 
-app.listen(5100, () => console.log("Server running on port 5100"));
+const port = process.env.PORT || 5100;
+app.listen(port, () => console.log(`Server running on port ${port}...`));
