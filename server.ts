fc7dc2148f5541a55a09f52eb383074ed8df37cd
Arvin Buid <arvinbuid01@gmail.com>
2025-03-12 17:07:34 +0800

    Get single job.

--- server.ts
+++ server.ts
@@ -24,11 +24,26 @@ app.post("/", (req, res) => {
   res.json({message: "Data received", data: req.body});
 });
 
-// GET, POST jobs
+// GET all jobs
 app.get("/api/v1/jobs", (req, res) => {
   res.status(200).json({jobs});
 });
 
+// GET single job
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const {id} = req.params;
+
+  const job = jobs.find((job) => job.id === id);
+
+  if (!job) {
+    res.status(404).json({message: `No job with id ${id}`});
+    return;
+  }
+
+  res.status(200).json({job});
+});
+
+// POST create job
 app.post("/api/v1/jobs", (req, res) => {
   const {name, company} = req.body;
 
