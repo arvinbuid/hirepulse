b449b0942cda1c82d9b6a80646635128aa49495d
Arvin Buid <arvinbuid01@gmail.com>
2025-03-14 20:03:53 +0800

    Hash password & hash password utils.

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -2,13 +2,18 @@ import {Request, Response, NextFunction} from "express";
 import {StatusCodes} from "http-status-codes";
 
 import User from "../models/userModel.ts";
+import {hashPassword} from "../utils/passwordUtils.ts";
 
 export const postRegister = async (req: Request, res: Response) => {
   const isFirstUser = (await User.countDocuments()) === 0;
   req.body.role = isFirstUser ? "admin" : "user";
 
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({message: "User created.", user});
+  // Hash password
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
+  await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({message: "User created."});
 };
 
 export const postLogin = async (req: Request, res: Response) => {
