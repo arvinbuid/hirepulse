69f98fef58993dab7d77bdb5984f761cbc590985
Arvin Buid <arvinbuid01@gmail.com>
2025-03-17 23:57:12 +0800

    Add toast message.

--- frontend/src/actions/registerActions.ts
+++ frontend/src/actions/registerActions.ts
@@ -1,4 +1,7 @@
 import {redirect} from "react-router-dom";
+import {toast} from "react-toastify";
+import {AxiosError} from "axios";
+
 import customFetch from "../utils/customFetch";
 
 export const action = async ({request}: {request: Request}) => {
@@ -7,8 +10,11 @@ export const action = async ({request}: {request: Request}) => {
 
   try {
     await customFetch.post("/auth/register", data);
+    toast.success("Registration successful");
     return redirect("/login");
   } catch (error) {
+    const axiosError = error as AxiosError<{message: string}>;
+    toast.error(axiosError.response?.data?.message);
     return error;
   }
 };
