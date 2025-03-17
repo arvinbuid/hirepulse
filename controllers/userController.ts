022066a6077e20304bfef568f1c1b71b036655c4
Arvin Buid <arvinbuid01@gmail.com>
2025-03-17 12:35:32 +0800

    Get current user.

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,8 +1,11 @@
 import {StatusCodes} from "http-status-codes";
 import {Request, Response} from "express";
 
+import User from "../models/UserModel.ts";
+
 export const getCurrentUser = async (req: Request, res: Response) => {
-  res.status(StatusCodes.OK).json({message: "get current user."});
+  const user = await User.findOne({_id: req.user?.userId}).select("-password");
+  res.status(StatusCodes.OK).json({user});
 };
 
 export const getApplicationStats = async (req: Request, res: Response) => {
