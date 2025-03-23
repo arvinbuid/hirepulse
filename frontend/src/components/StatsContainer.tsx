ed43cc610e7f07ae9ed9f1defc2949ea8072be33
Arvin Buid <arvinbuid01@gmail.com>
2025-03-23 12:23:55 +0800

    Stats container.

--- frontend/src/components/StatsContainer.tsx
+++ frontend/src/components/StatsContainer.tsx
@@ -1,11 +1,43 @@
+import {FaBug, FaCalendarCheck, FaSuitcaseRolling} from "react-icons/fa";
 import Wrapper from "../assets/wrappers/StatsContainer";
+import StatItem from "./StatItem";
 
 interface StatsContainerProps {
   defaultStats: {pending: number; interview: number; declined: number};
 }
 
-const StatsContainer = (defaultStats: StatsContainerProps) => {
-  return <Wrapper>Stats Container</Wrapper>;
+const StatsContainer = ({defaultStats}: StatsContainerProps) => {
+  const stats = [
+    {
+      title: "pending applications",
+      count: defaultStats?.pending || 0,
+      icon: <FaSuitcaseRolling />,
+      color: "#f59e0b",
+      bcg: "#fef3c7",
+    },
+    {
+      title: "interviews scheduled",
+      count: defaultStats?.interview || 0,
+      icon: <FaCalendarCheck />,
+      color: "#647acb",
+      bcg: "#e0e8f9",
+    },
+    {
+      title: "jobs declined",
+      count: defaultStats?.declined || 0,
+      icon: <FaBug />,
+      color: "#d66a6a",
+      bcg: "#ffeeee",
+    },
+  ];
+
+  return (
+    <Wrapper>
+      {stats.map((stat) => {
+        return <StatItem key={stat.title} {...stat} />;
+      })}
+    </Wrapper>
+  );
 };
 
 export default StatsContainer;
