10c8799da8858cc72b23cb50ddc180a54b3b9fb5
Arvin Buid <arvinbuid01@gmail.com>
2025-03-25 18:26:16 +0800

    Update error component.

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,21 +1,30 @@
-import {Link, useRouteError} from "react-router-dom";
+import {Link, useRouteError, isRouteErrorResponse, useNavigate} from "react-router-dom";
 import Wrapper from "../assets/wrappers/ErrorPage";
 import NotFound from "../assets/images/not-found.svg";
 
 const Error = () => {
   const error = useRouteError();
+  const navigate = useNavigate();
 
-  if (error.status === 404) {
-    return (
-      <Wrapper>
-        <div>
-          <img src={NotFound} alt='Not found' />
-          <h3>Ohh! Page Not Found</h3>
-          <p>We can't seem to find the page that you are looking for.</p>
-          <Link to='/'>Back to Home</Link>
-        </div>
-      </Wrapper>
-    );
+  const backToPreviousPage = () => {
+    navigate(-1);
+  };
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 404) {
+      return (
+        <Wrapper>
+          <div>
+            <img src={NotFound} alt='Not found' />
+            <h3>Ohh! Page Not Found</h3>
+            <p>We can't seem to find the page that you are looking for.</p>
+            <button onClick={backToPreviousPage} className='btn'>
+              Back
+            </button>
+          </div>
+        </Wrapper>
+      );
+    }
   }
 
   return (
